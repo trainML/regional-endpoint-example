2cd0b22b3b92932bef3ee99a17ca57f62e85c220
Adam Kowalski <adam@trainml.ai>
2023-03-15 10:27:31 -0500

    switching to run on port 80 by default

--- start_endpoint.sh
+++ start_endpoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-uvicorn api:app --host 0.0.0.0 --port 80 &
+uvicorn api:app --host 0.0.0.0 --port 8080 &
 envsubst '${TRAINML_MODEL_PATH} ${PORT}' < $TRAINML_MODEL_PATH/nginx.template > /etc/nginx/nginx.conf
 nginx
